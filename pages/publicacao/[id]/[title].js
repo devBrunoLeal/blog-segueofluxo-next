a82196ba1d36654abe6d276b229653c33ea13960
devBrunoLeal <brunonovato22@gmail.com>
2021-03-10 23:50:30 -0300

    up

--- pages/publicacao/[id]/[title].js
+++ pages/publicacao/[id]/[title].js
@@ -42,6 +42,7 @@ export async function getServerSidePaths() {
 
 const Publicacao = ({ post }) => {
   
+  
   let stringona = '<script type="application/ld+json"> { "@context": "https://schema.org", "@type": "NewsArticle", "mainEntityOfPage": { "@type": "WebPage", "@id": "https://google.com/article" }, "headline": '+'"'+post.title.rendered+'"'+', "image": [ '+'"'+post["better_featured_image"]["source_url"]+'"'+', '+'"'+post["better_featured_image"]["source_url"]+'"'+', '+'"'+post["better_featured_image"]["source_url"]+'"'+' ], "datePublished": '+'"'+post.date+'"'+', "dateModified": '+'"'+post.modified+'"'+', "author": { "@type": "Person", "name": '+'"'+post._embedded.author[0].name+'"'+' }, "publisher": { "@type": "Organization", "name": "Segue o fluxo", "logo": { "@type": "ImageObject", "url": "https://api.segueofluxo.com/wp-content/uploads/2021/02/70871127_822907708123959_3608893476449550336_n.png" } } } </script>'
   
   if (post) {
