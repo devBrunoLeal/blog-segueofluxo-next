5da5748dae6c2f0478f68f3b3ced3dbaace98cc4
devBrunoLeal <brunonovato22@gmail.com>
2021-03-15 23:35:30 -0300

    update

--- pages/publicacao/[id]/[title].js
+++ pages/publicacao/[id]/[title].js
@@ -19,6 +19,10 @@ export async function getStaticProps(context) {
   data.resumo = data.resumo.replace("</p>", "");
   data.resumo = data.resumo.replace(/\n/g, "");
 
+
+   data.title.rendered = data.title.rendered.replace(/&#8220;/g,'“');
+   data.title.rendered = data.title.rendered.replace(/&#8221;/g,'”')
+
   return {
     props: { post: data },
     revalidate: 1,
@@ -43,6 +47,9 @@ export async function getStaticPaths() {
 }
 
 const Publicacao = ({ post }) => {
+
+
+
   if (post) {
     return (
       <>
@@ -50,60 +57,38 @@ const Publicacao = ({ post }) => {
         <div className="main max" id="main" role="main">
           <Head>
             <meta charSet="utf-8" />
-            <meta
-              name="google-site-verification"
-              content="4wWHEX4N9ycRVz2DsqPZnck63Lkw_bOUxNAIJcB6XO4"
-            />
+            <meta name="google-site-verification" content="4wWHEX4N9ycRVz2DsqPZnck63Lkw_bOUxNAIJcB6XO4" />
             <meta name="language" content="pt-BR" />
             <title>{post.title.rendered}</title>
             <meta name="description" content={post.resumo} />
-            <meta
-              name="robots"
-              content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
-            />
+            <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
             <meta name="author" content={post._embedded.author[0].name} />
             <meta name="keywords" content={post._embedded["wp:term"][1].map((tag) => tag.name)} />
             <meta property="og:site_name" content="Segue o fluxo" />
 
             <meta property="article:published_time" content={post.date} />
             <meta property="article:modified_time" content={post.modified} />
-            <meta
-              property="article:publisher"
-              content="https://www.facebook.com/segueofluxooriginal/"
-            />
+            <meta property="article:publisher" content="https://www.facebook.com/segueofluxooriginal/" />
             <meta property="article:author" content={post._embedded.author[0].name} />
             <meta property="og:image:width" content="758" />
             <meta property="og:image:height" content="424" />
 
             <meta property="og:type" content="page" />
-            <meta
-              property="og:url"
-              content={"https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug}
-            />
+            <meta property="og:url" content={"https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug} />
 
             <meta property="og:title" content={post.title.rendered} />
             <meta property="og:image" content={post["better_featured_image"]["source_url"]} />
             <meta property="og:description" content={post.resumo} />
 
             <meta property="article:author" content={post._embedded.author[0].name} />
-            <meta
-              property="og:image:secure_url"
-              itemprop="image"
-              content={post["better_featured_image"]["source_url"]}
-            />
+            <meta property="og:image:secure_url" itemprop="image" content={post["better_featured_image"]["source_url"]} />
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:site" content="@segueofluxonews" />
             <meta name="twitter:title" content={post.title.rendered} />
             <meta name="twitter:creator" content="@segueofluxonews" />
             <meta name="twitter:description" content={post.resumo} />
             <div id="fb-root"></div>
-            <script
-              async
-              defer
-              crossorigin="anonymous"
-              src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0"
-              nonce="xF3GKLHk"
-            ></script>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0" nonce="xF3GKLHk"></script>
             <script
               type="application/ld+json"
               dangerouslySetInnerHTML={{
@@ -146,53 +131,22 @@ const Publicacao = ({ post }) => {
               <div className="wrapper">
                 <section style={{ width: "100%" }} className="content-area col-content">
                   <header className="heading max">
-                    <a
-                      href={
-                        "/page/1/categoria/" +
-                        post.categories[0] +
-                        "/" +
-                        post._embedded["wp:term"][0][0].name
-                      }
-                      className="title-cat"
-                    >
+                    <a href={"/page/1/categoria/" + post.categories[0] + "/" + post._embedded["wp:term"][0][0].name} className="title-cat">
                       {post._embedded["wp:term"][0][0].name}
                     </a>
-                    <h1
-                      className="title title--big"
-                      dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-                    ></h1>
+                    <h1 className="title title--big" dangerouslySetInnerHTML={{ __html: post.title.rendered }}></h1>
                     <div className="sub-titulo">
-                      <p
-                        className="sub-titulo"
-                        dangerouslySetInnerHTML={{ __html: post.resumo }}
-                      ></p>
+                      <p className="sub-titulo" dangerouslySetInnerHTML={{ __html: post.resumo }}></p>
                     </div>
                     <hr />
                     <span className="byline">
-                      <a
-                        className="byline__item"
-                        href={
-                          "/page/1/user/" +
-                          post._embedded.author[0].id +
-                          "/" +
-                          post._embedded.author[0].slug
-                        }
-                      >
-                        <img
-                          className="icon"
-                          src={post._embedded.author[0].avatar_urls["48"]}
-                          alt=""
-                        />
+                      <a className="byline__item" href={"/page/1/user/" + post._embedded.author[0].id + "/" + post._embedded.author[0].slug}>
+                        <img className="icon" src={post._embedded.author[0].avatar_urls["48"]} alt="" />
 
                         {post._embedded.author[0].name}
                       </a>
                       <span className="byline__item">
-                        <img
-                          className="icon"
-                          style={{ maxWidth: "9px", paddingBottom: "4px" }}
-                          src={"../../assets/timer-svgrepo-com (1).svg"}
-                          alt=""
-                        />
+                        <img className="icon" style={{ maxWidth: "9px", paddingBottom: "4px" }} src={"../../assets/timer-svgrepo-com (1).svg"} alt="" />
                         <Moment format="DD/MM/YYYY" date={post.date} />
                       </span>
                       <div className="share">
@@ -200,54 +154,18 @@ const Publicacao = ({ post }) => {
                         <a
                           className="share__button"
                           href={
-                            "https://www.facebook.com/sharer.php?display=page&u=https://segueofluxo.com/publicacao/" +
-                            post.id +
-                            "/" +
-                            post.slug +
-                            "%3Futm_medium%3Dshare-bar%26utm_source%3Dfacebook"
+                            "https://www.facebook.com/sharer.php?display=page&u=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug + "%3Futm_medium%3Dshare-bar%26utm_source%3Dfacebook"
                           }
                           target="blank"
                           rel="noopener"
                         >
-                          <img
-                            className="icon"
-                            src="../../assets/facebook.png"
-                            alt="Compartilhar no Facebook"
-                          />
+                          <img className="icon" src="../../assets/facebook.png" alt="Compartilhar no Facebook" />
                         </a>
-                        <a
-                          className="share__button"
-                          href={
-                            "https://twitter.com/intent/tweet?url=https://segueofluxo.com/publicacao/" +
-                            post.id +
-                            "/" +
-                            post.slug
-                          }
-                          target="blank"
-                          rel="noopener"
-                        >
-                          <img
-                            className="icon"
-                            src="../../assets/twitter.png"
-                            alt="Compartilhar no Twitter"
-                          />
+                        <a className="share__button" href={"https://twitter.com/intent/tweet?url=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug} target="blank" rel="noopener">
+                          <img className="icon" src="../../assets/twitter.png" alt="Compartilhar no Twitter" />
                         </a>
-                        <a
-                          className="share__button"
-                          href={
-                            "https://api.whatsapp.com/send?text=https://segueofluxo.com/publicacao/" +
-                            post.id +
-                            "/" +
-                            post.slug
-                          }
-                          target="blank"
-                          rel="noopener"
-                        >
-                          <img
-                            className="icon"
-                            src="../../assets/wp.png"
-                            alt="Compartilhar no Whatsapp"
-                          />
+                        <a className="share__button" href={"https://api.whatsapp.com/send?text=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug} target="blank" rel="noopener">
+                          <img className="icon" src="../../assets/wp.png" alt="Compartilhar no Whatsapp" />
                         </a>
                       </div>
                     </span>
@@ -258,15 +176,7 @@ const Publicacao = ({ post }) => {
                     className="content-area__body content max post-100506 post type-post status-publish format-standard has-post-thumbnail hentry category-rap-nacional tag-lancamentos"
                   >
                     <figure className="wp-block-image size-large">
-                      <img
-                        loading="lazy"
-                        width="758"
-                        height="424"
-                        src={post["better_featured_image"]["source_url"]}
-                        alt=""
-                        className="wp-image-100507"
-                        sizes="(max-width: 758px) 100vw, 758px"
-                      />
+                      <img loading="lazy" width="758" height="424" src={post["better_featured_image"]["source_url"]} alt="" className="wp-image-100507" sizes="(max-width: 758px) 100vw, 758px" />
                     </figure>
                     <div
                       dangerouslySetInnerHTML={{
@@ -279,11 +189,7 @@ const Publicacao = ({ post }) => {
                       <li>
                         {post._embedded["wp:term"][1].map((tag) =>
                           tag ? (
-                            <a
-                              style={{ marginRight: "5px" }}
-                              href={"/page/1/tags/" + tag.id + "/" + tag.slug}
-                              rel="tag"
-                            >
+                            <a style={{ marginRight: "5px" }} href={"/page/1/tags/" + tag.id + "/" + tag.slug} rel="tag">
                               {tag.name}
                             </a>
                           ) : (
@@ -297,77 +203,27 @@ const Publicacao = ({ post }) => {
                       <a
                         className="share__button"
                         href={
-                          "https://www.facebook.com/sharer.php?display=page&u=https://segueofluxo.com/publicacao/" +
-                          post.id +
-                          "/" +
-                          post.slug +
-                          "%3Futm_medium%3Dshare-bar%26utm_source%3Dfacebook"
+                          "https://www.facebook.com/sharer.php?display=page&u=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug + "%3Futm_medium%3Dshare-bar%26utm_source%3Dfacebook"
                         }
                         target="blank"
                         rel="noopener"
                       >
-                        <img
-                          className="icon"
-                          src="../../assets/facebook.png"
-                          alt="Compartilhar no Facebook"
-                        />
+                        <img className="icon" src="../../assets/facebook.png" alt="Compartilhar no Facebook" />
                       </a>
-                      <a
-                        className="share__button"
-                        href={
-                          "https://twitter.com/intent/tweet?url=https://segueofluxo.com/publicacao/" +
-                          post.id +
-                          "/" +
-                          post.slug
-                        }
-                        target="blank"
-                        rel="noopener"
-                      >
-                        <img
-                          className="icon"
-                          src="../../assets/twitter.png"
-                          alt="Compartilhar no Twitter"
-                        />
+                      <a className="share__button" href={"https://twitter.com/intent/tweet?url=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug} target="blank" rel="noopener">
+                        <img className="icon" src="../../assets/twitter.png" alt="Compartilhar no Twitter" />
                       </a>
-                      <a
-                        className="share__button"
-                        href={
-                          "https://api.whatsapp.com/send?text=https://segueofluxo.com/publicacao/" +
-                          post.id +
-                          "/" +
-                          post.slug
-                        }
-                        target="blank"
-                        rel="noopener"
-                      >
-                        <img
-                          className="icon"
-                          src="../../assets/wp.png"
-                          alt="Compartilhar no Whatsapp"
-                        />
+                      <a className="share__button" href={"https://api.whatsapp.com/send?text=https://segueofluxo.com/publicacao/" + post.id + "/" + post.slug} target="blank" rel="noopener">
+                        <img className="icon" src="../../assets/wp.png" alt="Compartilhar no Whatsapp" />
                       </a>
                     </div>
                     <div className="author max">
                       <span className="author__avatar">
-                        <img
-                          src={post._embedded.author[0].avatar_urls["48"]}
-                          width="90"
-                          height="90"
-                          alt="user.name"
-                          className="avatar avatar-90 wp-user-avatar wp-user-avatar-90 alignnone photo"
-                        />
+                        <img src={post._embedded.author[0].avatar_urls["48"]} width="90" height="90" alt="user.name" className="avatar avatar-90 wp-user-avatar wp-user-avatar-90 alignnone photo" />
                       </span>
                       <div className="author__bio">
                         <b className="title title--smaller">
-                          <a
-                            href={
-                              "/page/1/user/" +
-                              post._embedded.author[0].id +
-                              "/" +
-                              post._embedded.author[0].slug
-                            }
-                            rel="author"
-                          >
+                          <a href={"/page/1/user/" + post._embedded.author[0].id + "/" + post._embedded.author[0].slug} rel="author">
                             {post._embedded.author[0].name}
                           </a>
                         </b>
@@ -396,12 +252,7 @@ const Publicacao = ({ post }) => {
                     <h4 className="title title--small title--upper">Comentários:</h4>
                     <div
                       className="fb-comments"
-                      data-href={
-                        "https://www.facebook.com/sharer/sharer.php?u=https://segueofluxo.com/publicacao/" +
-                        post.id +
-                        "/" +
-                        post.title.rendered
-                      }
+                      data-href={"https://www.facebook.com/sharer/sharer.php?u=https://segueofluxo.com/publicacao/" + post.id + "/" + post.title.rendered}
                       data-width="730"
                       data-numposts="5"
                     ></div>
@@ -489,16 +340,8 @@ const Publicacao = ({ post }) => {
                       <img className="icon" src="../../assets/instagram.png" alt="Instagram" />
                     </span>
                     <h4 className="title title--medium">Nos siga no Instagram também!</h4>
-                    <p>
-                      E não perca nenhuma novidade sobre o mundo do Funk. Nos siga clicando no botão
-                      abaixo.
-                    </p>
-                    <a
-                      className="title-cat"
-                      href="https://www.instagram.com/segueofluxooriginal/"
-                      target="_blank"
-                      rel="noopener"
-                    >
+                    <p>E não perca nenhuma novidade sobre o mundo do Funk. Nos siga clicando no botão abaixo.</p>
+                    <a className="title-cat" href="https://www.instagram.com/segueofluxooriginal/" target="_blank" rel="noopener">
                       Seguir!
                     </a>
                     <small className="instagram__note">
