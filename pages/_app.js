f33f811d4ebcd4bcf57cd9e99423f37de8b868ad
devBrunoLeal <brunonovato22@gmail.com>
2021-04-24 00:43:53 -0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,16 @@ function MyApp({ Component, pageProps }) {
 <meta name="theme-color" content="#1647b5"/>
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="apple-mobile-web-app-status-bar-style" content="#1647b5"/>
+
+{/*  Global site tag (gtag.js) - Google Analytics  */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){'{'}dataLayer.push(arguments){'}'}
+  gtag('js', new Date());
+
+  gtag('config', 'GA_MEASUREMENT_ID');
+</script>
      </Head>
     <Header></Header>
    <div className="main max" style={{background: 'white', maxWidth: '1120px', margin: 'auto'}} id="main" role="main">
