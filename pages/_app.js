fc49352d0dff623b589322dc04b37330e34c23b7
devBrunoLeal <brunonovato22@gmail.com>
2021-05-04 21:45:17 -0300

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Head from "next/head";
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import MetaTags from "react-meta-tags";
